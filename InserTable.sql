586f14b4d5f4208cf1353dfce1e590d453860dc0
Aryans <quispeloayza18@hotmail.com>
2024-04-23 09:47:07 -0500

    Insertar datos en las tablas.

---
+++ InserTable.sql
@@ -0,0 +1,43 @@
+INSERT INTO Gerente (idGerente, descGerente) 
+VALUES (1, 'Arnny');
+
+INSERT INTO Gerente (idGerente, descGerente) 
+VALUES (2, 'Snaydher');
+
+INSERT INTO Gerente (idGerente, descGerente) 
+VALUES (3, 'Said');
+
+INSERT INTO Gerente (idGerente, descGerente) 
+VALUES (4, 'Yuan');
+--------------------------------------------------------
+INSERT INTO Condicion (idCondicion, descCondicion) 
+VALUES (1, 'UNA ESTRELLA');
+INSERT INTO Condicion (idCondicion, descCondicion) 
+VALUES (2, 'DOS ESTRELLAS');
+INSERT INTO Condicion (idCondicion, descCondicion) 
+VALUES (3, 'TRES ESTRELLA');
+INSERT INTO Condicion (idCondicion, descCondicion) 
+VALUES (4, 'CUATRO ESTRELLAS');
+
+--------------------------------------------------------
+INSERT INTO Provincia (idProvincia, descProvincia) 
+VALUES (1, 'Lima');
+INSERT INTO Provincia (idProvincia, descProvincia) 
+VALUES (2, 'Callao');
+
+--------------------------------------------------------
+INSERT INTO Distrito (idDistrito, idProvincia, descDistrito) 
+VALUES (1, 1, 'Brenia');
+INSERT INTO Distrito (idDistrito, idProvincia, descDistrito) 
+VALUES (2, 2, 'Bellavista');
+
+--------------------------------------------------------
+
+INSERT INTO Sede(idSede, descSede) 
+VALUES (1,'Sede 1');
+
+INSERT INTO Sede(idSede, descSede) 
+VALUES (2,'Sede 2');
+
+INSERT INTO Sede(idSede, descSede) 
+VALUES (3,'Sede 3');
